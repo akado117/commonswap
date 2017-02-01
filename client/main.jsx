03cc4705bfeac1257c4e4090246d0163ae7686fd
Justin Foley <justin@modweb.io>
2017-02-01 13:45:06 -0500

    apollo/graphql hello world

--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,11 @@ import { render } from 'react-dom';
 
 import App from '../imports/ui/App.jsx';
 
+import ApolloClient from 'apollo-client';
+import { meteorClientConfig } from 'meteor/apollo';
+
+const client = new ApolloClient(meteorClientConfig());
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
---
+++ imports/api/resolvers.js
@@ -0,0 +1,7 @@
+export default {
+  Query: {
+    say(/* root, args, context */) {
+      return 'hello world';
+    },
+  },
+};

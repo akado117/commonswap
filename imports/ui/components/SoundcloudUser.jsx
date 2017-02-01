338cf1d9f8bbf414f5f576963c2cfaed269594f7
Justin Foley <justin@modweb.io>
2017-02-01 15:24:51 -0500

    soundcloud users component

--- imports/ui/components/SoundcloudUser.jsx
+++ imports/ui/components/SoundcloudUser.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import SoundcloudUsers from './components/SoundcloudUser';
 
 // Or a stateless functional component:
-const App = ({data}) => (
-    <div>
-      {data.say}
-      <SoundcloudUsers />
-    </div>
+const SoundcloudUsers = ({data}) => (
+    <div>{data.soundcloudUsers && data.soundcloudUsers.map((soundcloudUser) => soundcloudUser.username)}</div>
   );
 
 // Initialize GraphQL queries or mutations with the `gql` tag
-const query = gql`query { say }`;
+const query = gql`
+  query {
+    soundcloudUsers {
+      username
+    }
+  }`;
 //const MyMutation = gql`mutation MyMutation { addTodo(text: "Test 123") { id } }`;
 
 // We then can use `graphql` to pass the query results returned by MyQuery
 // to MyComponent as a prop (and update them as the results change)
-export default graphql(query)(App);
+export default graphql(query)(SoundcloudUsers);
 
 // Or, we can bind the execution of MyMutation to a prop
 // export default graphql(MyMutation)(MyComponent);

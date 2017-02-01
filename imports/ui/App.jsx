04afc0c497eb1de495faf9851a8f5c70a1f88f64
Justin Foley <justin@modweb.io>
2017-02-01 14:45:08 -0500

    hello world from graphql/apollo

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+//import React from 'react';
 
-export default () => (
-  <div>Hello World</div>
-);
+// export default () => (
+//   <div>Hello World</div>
+// );
+
+
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+// Or a stateless functional component:
+const App = ({data}) => (
+    <div>{data.say}</div>
+  );
+
+// Initialize GraphQL queries or mutations with the `gql` tag
+const query = gql`query { say }`;
+//const MyMutation = gql`mutation MyMutation { addTodo(text: "Test 123") { id } }`;
+
+// We then can use `graphql` to pass the query results returned by MyQuery
+// to MyComponent as a prop (and update them as the results change)
+export default graphql(query)(App);
+
+// Or, we can bind the execution of MyMutation to a prop
+// export default graphql(MyMutation)(MyComponent);

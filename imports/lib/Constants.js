d7c4c244defaba4276b3a14fdd44e3fb732f970b
timhh <timhh@sbcglobal.net>
2017-09-24 15:00:15 -0400

    Slight changes we made while at the office

--- imports/lib/Constants.js
+++ imports/lib/Constants.js
@@ -2,6 +2,7 @@ export default {
     actionTypes: {
         SAVE_ROOMIES: 'SAVE_ROOMIES',
         LOGIN_: 'LOGIN_',
+        SAVE_PROFILE: 'SAVE_PROFILE',
     },
     SUCCESS: 'SUCCESS',
     FAILURE: 'FAILURE',

13e627b862f071364523bf70fc9974b276a3d3a1
timhh <timhh@sbcglobal.net>
2017-10-22 21:49:54 -0400

    Beginning of the profile listing

--- imports/ui/pages/ViewProfile.jsx
+++ imports/ui/pages/ViewProfile.jsx
@@ -16,28 +16,41 @@ const profile = <FontIcon className="material-icons">person</FontIcon>;
 class ViewProfile extends React.Component {
     constructor() {
         super();
-        this.state = { };
+        this.state = {};
     }
 
     componentDidMount = () => { }
 
     render() {
         let internalComponent;
-        if(this.state.selectedIndex === 0){
-            internalComponent = <ProfileComponent getValueFunc={this.addValueOnChange}/>
+        if (this.state.selectedIndex === 0) {
+            internalComponent = <ProfileComponent getValueFunc={this.addValueOnChange} />
         } else if (this.state.selectedIndex === 1) {
-            internalComponent = <PlaceComponent getValueFunc={this.addValueOnChange}/>
+            internalComponent = <PlaceComponent getValueFunc={this.addValueOnChange} />
         }
 
         return (
             <section className="profile-view-container" >
                 <Navbar></Navbar>
+                <div className="container">
+                    <div className="col s12 z-depth-2 place-images">
+                        <div className="row">
+                            <div className="col s8 main-image">
+                                <img src="http://stretchflex.net/photos/apartment.jpeg" alt="" className="main-image" />
+                            </div>
+                            <div className="col s4 scroll-image">
+                                <img src="http://stretchflex.net/photos/apartment2.jpeg" alt="" className="main-image" />
+                                <img src="http://stretchflex.net/photos/apartment3.jpeg" alt="" className="main-image" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Footer></Footer>
             </section>
         );
     }
     testState = () => {
-        roomiesActions.saveRoom(this.state,this.props.dispatch)
+        roomiesActions.saveRoom(this.state, this.props.dispatch)
     }
 }
 export default ViewProfile

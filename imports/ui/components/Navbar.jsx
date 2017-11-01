738e43b55355bb3b8a0bf710d2d637cf7de30524
timhh <timhh@sbcglobal.net>
2017-10-31 20:24:48 -0400

    This is a change to the email function

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -15,13 +15,13 @@ class Navbar extends React.Component {
         return (
             <nav className="nav-wrapper">
                 <div>
-                    <a href="#!" className="brand-logo"><img src="http://stretchflex.net/photos/csLogo.jpg" alt="" style={{maxHeight:'64px'}}/></a>
+                    <a href="#!" className="brand-logo"><img src="" alt="" style={{maxHeight:'64px'}}/></a>
                     <a href="#" data-activates="mobile-demo" className="button-collapse"><FontIcon className="material-icons">menu</FontIcon></a>
                     <ul className="right hide-on-med-and-down">
-                        <li><Link to="/home">How It Works</Link></li>
-                        <li><Link to="/planner">Planner</Link></li>
-                        <li><Link to="/profile">Profile</Link></li>
-                        <li><Link to="/login">Login</Link></li>
+                        <li style={{color:'black'}}><Link to="/home">How It Works</Link></li>
+                        <li style={{color:'black'}}><Link to="/planner">Planner</Link></li>
+                        <li style={{color:'black'}}><Link to="/profile">Profile</Link></li>
+                        <li style={{color:'black'}}><Link to="/login">Login</Link></li>
                     </ul>
                     <ul className="side-nav" id="mobile-demo">
                         <li><Link to="/home">How It Works</Link></li>

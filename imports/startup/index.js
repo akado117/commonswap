9dff72ccf130e69683d527bd27bc2771f8f62435
timhh <timhh@sbcglobal.net>
2017-11-05 19:53:48 -0500

    nav changed color and logo, stripe token, Browse comp, planner styling, email working

--- imports/startup/index.js
+++ imports/startup/index.js
@@ -1,2 +1,3 @@
 import ServiceConfiguration from './ServiceConfiguration';
 import SlingShot from './SlingShot';
+//import Stripe from './Stripe';
--- imports/ui/pages/ViewProfile.js
+++ imports/ui/pages/ViewProfile.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
-import { connect }  from 'react-redux';
+import { connect } from 'react-redux';
 const _ = require('lodash')//required so it can be used easily in chrome dev tools.
 import DatePicker from 'material-ui/DatePicker';
 import FontIcon from 'material-ui/FontIcon';
 import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 import ProfileComponent from '../components/profileComps/ProfileComponent.js'
 import InterestsComponent from '../components/forms/ButtonArrayComp.js'
@@ -16,7 +19,16 @@ import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import ProfileActions from '../../actions/ProfileActions';
 
+
 const profile = <FontIcon className="material-icons">person</FontIcon>;
+
+const items = [
+    <MenuItem key={1} value={1} primaryText="1" />,
+    <MenuItem key={2} value={2} primaryText="2" />,
+    <MenuItem key={3} value={3} primaryText="3" />,
+    <MenuItem key={4} value={4} primaryText="4" />,
+    <MenuItem key={5} value={5} primaryText="5" />,
+  ];
 
 class ViewProfile extends React.Component {
     constructor(props) {
@@ -25,6 +37,7 @@ class ViewProfile extends React.Component {
             arrival: '',
             departure: '',
             notes: '',
+            guests: ''
         };
 
         this.updateArrival = this.updateArrival.bind(this);
@@ -32,22 +45,21 @@ class ViewProfile extends React.Component {
         this.updateNotes = this.updateNotes.bind(this);
     }
 
+    updateGuests = (event, index, guests) => this.setState({guests});
+
     updateArrival = (date) => {
-        console.log(date);
         this.setState({
-            arrival: date,
+            arrival: date.toLocaleDateString()
         });
     }
-    updateDeparture = (evt) => {
-        console.log(evt);
+    updateDeparture = (date) => {
         this.setState({
-            departure: evt.target.value
+            departure: date.toLocaleDateString()
         });
     }
-    updateNotes = (evt) => {
-        console.log(evt);
+    updateNotes = (notes) => {
         this.setState({
-            notes: evt.target.value
+            notes: notes
         });
     }
 
@@ -67,9 +79,9 @@ class ViewProfile extends React.Component {
     render() {
         let internalComponent;
         if (this.state.selectedIndex === 0) {
-            internalComponent = <ProfileComponent getValueFunc={this.addValueOnChange}/>
+            internalComponent = <ProfileComponent getValueFunc={this.addValueOnChange} />
         } else if (this.state.selectedIndex === 1) {
-            internalComponent = <PlaceComponent getValueFunc={this.addValueOnChange}/>
+            internalComponent = <PlaceComponent getValueFunc={this.addValueOnChange} />
         }
 
         return (
@@ -79,29 +91,29 @@ class ViewProfile extends React.Component {
                     <div className="col s12 z-depth-2 place-images">
                         <div className="row">
                             <div className="col s8 main-image">
-                                <img src="http://stretchflex.net/photos/apartment.jpeg" alt="" className="main-image"/>
+                                <img src="http://stretchflex.net/photos/apartment.jpeg" alt="" className="main-image" />
                             </div>
                             <div className="col s4 scroll-image">
-                                <img src="http://stretchflex.net/photos/apartment2.jpeg" alt="" className="main-image"/>
-                                <img src="http://stretchflex.net/photos/apartment3.jpeg" alt="" className="main-image"/>
+                                <img src="http://stretchflex.net/photos/apartment2.jpeg" alt="" className="main-image" />
+                                <img src="http://stretchflex.net/photos/apartment3.jpeg" alt="" className="main-image" />
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col s8">
                             <div className="row">
-                                <div className="col s12 z-depth-2" style={{paddingLeft: '0px', paddingRight: '0px'}}>
+                                <div className="col s12 z-depth-2" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
                                     <AppBar
                                         title={<span>About John</span>}
                                         showMenuIconButton={false}
-                                        style={{marginBottom: '10px'}}
+                                        style={{ marginBottom: '10px', zIndex: 0 }}
                                     />
                                     <div className="col s3">
                                         <img className="circle responsive-img"
-                                             src="http://stretchflex.net/photos/profileStock.jpeg" alt="profDemo"
-                                             style={{height: '140px', width: '140px'}}/>
+                                            src="http://stretchflex.net/photos/profileStock.jpeg" alt="profDemo"
+                                            style={{ height: '140px', width: '140px' }} />
                                     </div>
-                                    <div className="col s3" id="message" style={{top: '5px'}}>
+                                    <div className="col s3" id="message" style={{ top: '5px' }}>
                                         <div className="row">
                                             <div className="col s12">
                                                 <h5><strong>John</strong></h5>
@@ -177,31 +189,43 @@ class ViewProfile extends React.Component {
                             <div className="col s12 z-depth-2">
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <label htmlFor=""><FontIcon className="material-icons">date_range</FontIcon>
-                                            Arrival</label>
-                                        <input type="text" className="datepicker" value={this.state.arrival}
-                                               onChange={this.updateArrival}/>
                                         <DatePicker
                                             onChange={(nul, date) => this.updateArrival(date)}
-                                            floatingLabelText={<span><FontIcon className="material-icons">date_range</FontIcon>Arrival</span>}
-                                            //defaultDate={this.state.arrival}
-                                            //disableYearSelection={this.state.disableYearSelection}
+                                            floatingLabelText={<span><FontIcon className="material-icons">date_range</FontIcon> Arrival</span>}
+                                        //defaultDate={this.state.arrival}
+                                        //disableYearSelection={this.state.disableYearSelection}
                                         />
                                     </div>
                                     <div className="input-field col s12">
-                                        <label htmlFor=""><FontIcon className="material-icons">date_range</FontIcon>
-                                            Departure</label>
-                                        <input type="text" className="datepicker" value={this.state.departure}
-                                               onChange={this.updateDeparture}/>
+                                        <DatePicker
+                                            onChange={(nul, date) => this.updateDeparture(date)}
+                                            floatingLabelText={<span><FontIcon className="material-icons">date_range</FontIcon> Departure</span>}
+
+                                        />
                                     </div>
                                     <div className="input-field col s12">
-                                        <label htmlFor="textarea1">Add a message: </label>
-                                        <textarea id="textarea1" className="materialize-textarea" data-length="400"
-                                                  value={this.state.notes} onChange={this.updateNotes}></textarea>
+                                        <SelectField
+                                            value={this.state.guests}
+                                            onChange={this.updateGuests}
+                                            floatingLabelText={<span><FontIcon className="material-icons">person</FontIcon> Number of Guests</span>}
+                                            floatingLabelFixed={true}
+                                            //hintText="Guests"
+                                        >
+                                            {items}
+                                        </SelectField>
+                                    </div>
+                                    <div className="input-field col s12">
+                                        <TextField
+                                            hintText="Description"
+                                            floatingLabelText={<span><FontIcon className="material-icons">speaker_notes</FontIcon> Add a message</span>}
+                                            multiLine={true}
+                                            rows={5}
+                                            onChange={(nul, message) => this.updateNotes(message)}
+                                        />
                                     </div>
                                     <div className="col s12">
                                         <RaisedButton label="Request Swap" primary={true} fullWidth={true}
-                                                      onClick={() => this.requestSwap()}/>
+                                            onClick={() => this.requestSwap()} />
                                     </div>
                                 </div>
                             </div>
@@ -209,23 +233,23 @@ class ViewProfile extends React.Component {
                     </div>
                     <div className="col s8">
                         <div className="row">
-                            <div className="col s8 z-depth-2" style={{paddingLeft: '0px', paddingRight: '0px'}}>
+                            <div className="col s8 z-depth-2" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
                                 <AppBar
                                     title={<span>About John's Place</span>}
                                     showMenuIconButton={false}
-                                    style={{marginBottom: '10px'}}
+                                    style={{ marginBottom: '10px', zIndex: 0 }}
                                 />
                                 <div className="col s12">
                                     <div className="row">
-                                        <div className="col s4" style={{textAlign: 'center'}}>
+                                        <div className="col s4" style={{ textAlign: 'center' }}>
                                             <p>Entire Apt</p>
                                             <p><FontIcon className="material-icons large">home</FontIcon></p>
                                         </div>
-                                        <div className="col s4" style={{textAlign: 'center'}}>
+                                        <div className="col s4" style={{ textAlign: 'center' }}>
                                             <p>4 guests</p>
                                             <p><FontIcon className="material-icons large">people_outline</FontIcon></p>
                                         </div>
-                                        <div className="col s4" style={{textAlign: 'center'}}>
+                                        <div className="col s4" style={{ textAlign: 'center' }}>
                                             <p>1 Bedroom</p>
                                             <p><FontIcon className="material-icons large">hotel</FontIcon></p>
                                         </div>
@@ -304,9 +328,9 @@ class ViewProfile extends React.Component {
         );
     }
     requestSwap = () => {
-        alert('A name was submitted: ' + this.state.arrival);
-        console.log(this.state.arrival);
-        console.log(this.state.departure);
+        // console.log(this.state.notes);
+        // console.log(this.state.arrival);
+        // console.log(this.state.departure);
         this.props.profileActions.requestEmail({
             Arrival: this.state.arrival,
             Departure: this.state.departure,

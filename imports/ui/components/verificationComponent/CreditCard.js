1fee99f77de352d29a1b110e6dbb861612329fd7
akaidc2 <alex.kaidan@wendys.com>
2017-11-05 22:45:56 -0500

    removed unneeded package

--- imports/ui/components/verificationComponent/CreditCard.js
+++ imports/ui/components/verificationComponent/CreditCard.js
@@ -3,7 +3,6 @@ import Payment from 'payment';
 import { Row, Col, FormGroup, ControlLabel, Button, Alert } from 'react-bootstrap';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { getStripeToken } from '../../../modules/get-stripe-token.js';
-var FontAwesome = require('react-fontawesome');
 
 class CreditCard extends Component {
   constructor(props) {

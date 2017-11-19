d2f8d86b540d24484784e7eeb5636e26f2ff6b71
akaidc2 <alex.kaidan@wendys.com>
2017-11-18 20:34:23 -0500

    Calendar component 100% added in format helpers for dates

--- imports/ui/pages/Planner.jsx
+++ imports/ui/pages/Planner.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-const _ = require('lodash')
+import Rater from 'react-rater';
+import 'react-rater/lib/react-rater.css';
 import FontIcon from 'material-ui/FontIcon';
 import RaisedButton from 'material-ui/RaisedButton';
 import PropTypes from 'prop-types';
 import InfiniteCalendar, { Calendar, withMultipleRanges, EVENT_TYPES } from 'react-infinite-calendar';
+import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+import { FormateDates, ParseDates } from '../../helpers/DateHelpers';
 import '../../../node_modules/react-infinite-calendar/styles.css';
 import Navbar from '../components/Navbar';
-import AppBar from 'material-ui/AppBar';
-import TextField from 'material-ui/TextField';
 import '../../../node_modules/react-select/dist/react-select.css';
 import Footer from '../components/Footer';
-import Rater from 'react-rater'
-import 'react-rater/lib/react-rater.css'
 import BetaWarning from '../components/BetaWarning';
 import ProfileActions from '../../actions/ProfileActions';
 import PlaceActions from '../../actions/PlaceActions';
+
 
 const styles = {
     button: {
@@ -50,7 +51,7 @@ class Planner extends React.Component {
         this.state = {
             controlledDate: [new Date()],
             testDates: [new Date(),new Date(), new Date()],
-            selectedDates: props.place.place.availableDates || [],
+            selectedDates: ParseDates(props.place.place.availableDates || []),
             imgsData: [
                 {
                     url: 'http://stretchflex.net/photos/apartment.jpeg'
@@ -69,7 +70,7 @@ class Planner extends React.Component {
 
     componentDidUpdate = (prevProps) => {
         if (!prevProps.place.place._id && this.props.place.place._id) { //set dates from newly logged in user
-            const selectedDates = this.props.place.place.availableDates || [];
+            const selectedDates = ParseDates(this.props.place.place.availableDates || []);
             this.setState({ selectedDates });
         }
     }
@@ -94,7 +95,7 @@ class Planner extends React.Component {
     }
 
     saveDates = () => {
-        this.props.placeActions.updatePlaceDates(this.state.selectedDates || []);
+        this.props.placeActions.updatePlaceDates(FormateDates(this.state.selectedDates || []));
     }
 
     onCalendarSelect = (selectedDates, eventData) => {

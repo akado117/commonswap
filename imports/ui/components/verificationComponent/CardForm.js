b58b6413fa14b4631576ad380243b5aea76ca4c8
timhh <timhh@sbcglobal.net>
2017-11-27 20:50:09 -0500

    Card form UI changes

--- imports/ui/components/verificationComponent/CardForm.js
+++ imports/ui/components/verificationComponent/CardForm.js
@@ -22,8 +22,31 @@ class CardForm extends Component {
     render() {
         return (
             <div>
-                <Card ref={card => (this.card = card)} />
-                <RaisedButton label="Sign Up" primary={true} onClick={this.handleSubmit}/>
+                <AppBar
+                    title={<span><FontIcon className="material-icons">lock</FontIcon> Billing Information</span>}
+                    showMenuIconButton={false}
+                    style={{ marginBottom: '10px', zIndex: '0' }}
+                />
+                <div className="row">
+                    <div className="col l8" style={{ marginBottom: '1.5rem', marginTop: '1.5rem' }}>
+                        <Card ref={card => (this.card = card)} />
+                    </div>
+                    <div className="col l12">
+                        <RaisedButton label="Update Card" primary={true} onClick={this.handleSubmit} />
+                    </div>
+                    {/* <div className="col l12" style={{ marginTop: '1.5rem' }}>
+                        <div className="col l12" style={{ color: 'red' }} >
+                            Learn about available cities weekly!
+                        </div>
+                        <div className="col l6 input-field inline">
+                            <input type="text" className="" id="short-desc" />
+                            <label htmlFor="short-desc"><i className="fa fa-mail" aria-hidden="true"></i>Email</label>
+                        </div>
+                        <div className="col l12">
+                            <RaisedButton label="Sign up" primary={true} onClick={this.handleSubmit} />
+                        </div>
+                    </div> */}
+                </div>
             </div>
         );
     }

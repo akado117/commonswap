62cc4d2f8e1a5bd7314769e97520b521e8efd5aa
timhh <timhh@sbcglobal.net>
2017-12-13 20:44:50 -0500

    lastest code

--- client/ui/components/verificationComponent/CardForm.js
+++ client/ui/components/verificationComponent/CardForm.js
@@ -7,6 +7,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import handleSignup from '../../../../imports/modules/signup';
+import ProfileActions from '../../actions/ProfileActions';
 
 class CardForm extends Component {
     constructor(props) {
@@ -14,7 +15,9 @@ class CardForm extends Component {
     }
 
     componentDidMount() {
-        //handleSignup({ component: this });
+         const data = this.props.profileActions.retrieveCardInfo();
+        console.log("card info");
+        console.log(data);
     }
 
     handleSubmit = (event) => {
@@ -30,6 +33,19 @@ class CardForm extends Component {
                     showMenuIconButton={false}
                     style={{ marginBottom: '10px', zIndex: '0' }}
                 />
+                <div className="row">
+                    <div className="col s12">
+                        <p></p>
+                    </div>
+                    {/* <div className="col s12">
+                        <div className="col s6">
+                            {oldType}
+                        </div>
+                        <div className="col s6">
+                            {lastNum}
+                        </div>
+                    </div> */}
+                </div>
                 <div className="row">
                     <div className="col l8" style={{ marginBottom: '1.5rem', marginTop: '1.5rem' }}>
                         <Card ref={card => (this.card = card)} />
@@ -53,13 +69,14 @@ function mapStateToProps(state) {
   
   function mapDispatchToProps(dispatch) {
     return {
-  
+        profileActions: bindActionCreators(ProfileActions, dispatch),
     };
   }
   
   CardForm.propTypes = {
     user: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired,
+    profileActions: PropTypes.object.isRequired,
   };
 
   export default connect(mapStateToProps, mapDispatchToProps)(CardForm);

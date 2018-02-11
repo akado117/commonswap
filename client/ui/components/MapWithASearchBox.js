63a2321222982e6e618a023db5387dc1506525be
takoda117 <takoda117@gmail.com>
2018-02-10 20:59:23 -0500

    updated a crap ton of functionality within stand alone search bar and the maps with a search bar component.

--- client/ui/components/MapWithASearchBox.js
+++ client/ui/components/MapWithASearchBox.js
@@ -5,7 +5,7 @@ import { compose, withProps, lifecycle, defaultProps, withHandlers } from 'recom
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import PropTypes from 'prop-types';
 const { SearchBox } = require('react-google-maps/lib/components/places/SearchBox');
-import { buildMarkerObj } from '../../../imports/helpers/DataHelpers';
+import { buildMarkerObj, buildBoundsRange } from '../../../imports/helpers/DataHelpers';
 
 const centerMarker = {
     path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
@@ -16,9 +16,37 @@ const centerMarker = {
     strokeWeight: 14,
 };
 
+function getSearchBox(props, bounds) {
+    return (<SearchBox
+        ref={props.onSearchBoxMounted}
+        bounds={bounds}
+        controlPosition={google.maps.ControlPosition.TOP_LEFT}
+        onPlacesChanged={props.onPlacesChanged}
+    >
+        <input
+            type="text"
+            placeholder="Enter your area"
+            style={{
+                boxSizing: 'border-box',
+                border: '1px solid white',
+                width: '240px',
+                height: '32px',
+                marginTop: '12px',
+                padding: '0 12px',
+                borderRadius: '3px',
+                boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+                fontSize: '14px',
+                outline: 'none',
+                textOverflow: 'ellipses',
+                backgroundColor: 'white',
+            }}
+        />
+    </SearchBox>)
+}
+
 const MapSearchBox = compose(
     withProps(props => ({
-        googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDB1VkVvNXQUiKRzjVJoWfsyrusO5pkAWE&v=3.exp&libraries=geometry,drawing,places',
+        googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=' + Meteor.settings.public.googleKeys.googleMapsApiKey + '&v=3.exp&libraries=geometry,drawing,places',
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div className="map-wrapper" />,
         mapElement: <div style={{ height: `100%` }} id="map-canvas" />,
@@ -37,14 +65,15 @@ const MapSearchBox = compose(
     lifecycle({
         componentWillMount() {
             const refs = {};
-            const { lat, lng } = this.props.coords || (this.props.place && this.props.place.coords) || {};
+            const { lat, lng } = this.props.coords || (this.props.place && this.props.place.coords) || { lat: 41.9, lng: -87.624 };
             const initialMarker = lat !== undefined && lng !== undefined ? [buildMarkerObj({ lat, lng })] : [];
+            const boundPoints = buildBoundsRange({ lat, lng }, window.google.maps.LatLng, 1);
 
             this.setState({
-                bounds: null,
+                bounds: new window.google.maps.LatLngBounds(boundPoints.sw, boundPoints.ne),
                 center: {
-                    lat: lat || 41.9,
-                    lng: lng || -87.624,
+                    lat,
+                    lng,
                 },
                 markers: initialMarker,
                 injectedMarkers: this.props.markers || [],
@@ -70,23 +99,32 @@ const MapSearchBox = compose(
                     const nextCenter = get(nextMarkers, '0.position', this.state.center);
 
                     this.setState({
+                        bounds: this.state.buildNewBoundsObject(nextCenter),
                         center: nextCenter,
                         markers: nextMarkers,
                     });
                 },
+                buildNewBoundsObject: (nextCenter) => {
+                    const boundsCenter = {
+                        lat: typeof nextCenter.lat === 'function' ? nextCenter.lat() : nextCenter.lat,
+                        lng: typeof nextCenter.lng === 'function' ? nextCenter.lng() : nextCenter.lng,
+                    };
+                    const { sw, ne } = buildBoundsRange(boundsCenter, window.google.maps.LatLng, 0);
+                    return new window.google.maps.LatLngBounds(sw, ne);
+                },
                 onPlacesChanged: () => {
                     const places = refs.searchBox.getPlaces();
 
-                    places.map(({ address_components, geometry: { location } }) => {
-                        this.props.onSetLocation({
-                            lat: location.lat(),
-                            lng: location.lng(),
-                        });
-                    });
+                    this.props.onSearchComplete(places);
                     this.state.setNewCenter(places);
                     // refs.map.fitBounds(bounds);
                 },
             })
+        },
+        componentWillUpdate(nextProps) {
+            if (nextProps.externalMarkers !== this.props.externalMarkers) {
+                this.state.bounds = this.state.buildNewBoundsObject(this.state.center);
+            }
         },
         componentDidUpdate(prevProps) {
             const { place } = prevProps;
@@ -98,10 +136,18 @@ const MapSearchBox = compose(
                 }];
                 this.state.setNewCenter(places);
             }
+            if (prevProps.coords.lat !== this.props.coords.lat && prevProps.coords.lng !== this.props.coords.lng) {
+                const places = [{
+                    geometry: {
+                        location: this.props.coords,
+                    },
+                }];
+                this.state.setNewCenter(places);
+            }
         },
     }),
 )((props) => {
-    const bounds = new window.google.maps.LatLngBounds();
+    const { bounds } = props;
     const map = props.getMapRef();
     const markers = props.markers.map((marker, index) => {
         if (marker.position) {
@@ -111,7 +157,7 @@ const MapSearchBox = compose(
                     typeof marker.position.lng === 'function' ? marker.position.lng() : marker.position.lng,
                 ));
             }
-            return <Marker key={`internal-marker-${index}`} position={marker.position} />;
+            return <Marker key={'internal-marker-' + index} position={marker.position} />;
         }
     });
     const externalMarks = props.externalMarkers.map((marker, index) => {
@@ -122,7 +168,7 @@ const MapSearchBox = compose(
                     marker.coords.lng,
                 ));
             }
-            return <Marker key={`external-marker-${index}`} position={marker.coords} onClick={() => props.onMarkerClick(marker)}/>;
+            return <Marker key={'external-marker-' + index} position={marker.coords} onClick={() => props.onMarkerClick(marker)}/>;
         }
     });
     const fullMarkerArr = markers.concat(externalMarks);
@@ -130,76 +176,29 @@ const MapSearchBox = compose(
         map.fitBounds(bounds);
         map.panToBounds(bounds);
     }
-    return <GoogleMap
-        ref={props.onMapMounted}
-        defaultZoom={15}
-        center={props.center}
-        onBoundsChanged={props.onBoundsChanged}
-    >
-        <SearchBox
-            ref={props.onSearchBoxMounted}
-            bounds={props.bounds}
-            controlPosition={google.maps.ControlPosition.TOP_LEFT}
-            onPlacesChanged={props.onPlacesChanged}
+    return (
+        <GoogleMap
+            ref={props.onMapMounted}
+            defaultZoom={15}
+            center={props.center}
+            onBoundsChanged={props.onBoundsChanged}
         >
-            <input
-                type="text"
-                placeholder="Enter your area"
-                style={{
-                    boxSizing: 'border-box',
-                    border: '1px solid white',
-                    width: '240px',
-                    height: '32px',
-                    marginTop: '12px',
-                    padding: '0 12px',
-                    borderRadius: '3px',
-                    boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
-                    fontSize: '14px',
-                    outline: 'none',
-                    textOverflow: 'ellipses',
-                    backgroundColor: 'white',
-                }}
-            />
-        </SearchBox>
-        {fullMarkerArr}
-    </GoogleMap>
+            {!props.hideSearchBar ? getSearchBox(props, bounds) : ''}
+            {fullMarkerArr}
+        </GoogleMap>);
 });
 
-class MapComponent extends React.PureComponent {
-
-    constructor(props) {
-        super(props);
-        this.state = { isMarkerShown: false };
-    }
-
-    componentDidMount() {
-        this.delayedShowMarker();
-    }
-
-    delayedShowMarker = () => {
-        setTimeout(() => {
-            this.setState({ isMarkerShown: true });
-        }, 3000);
-    }
-
-    render() {
-        return (
-            <MapSearchBox
-                isMarkerShown={this.state.isMarkerShown}
-            />
-        );
-    }
-}
-
-MapComponent.propTypes = {
+MapSearchBox.propTypes = {
     className: PropTypes.string,
     latitude: PropTypes.string,
     longitude: PropTypes.string,
     externalMarkers: PropTypes.array,
     resizeBounds: PropTypes.bool,
+    hideSearchBar: PropTypes.bool,
+    onSearchComplete: PropTypes.func.isRequired,
 };
 
-MapComponent.defaultProps = {
+MapSearchBox.defaultProps = {
     className: '',
     externalMarkers: [],
     resizeBounds: false,
--- client/ui/components/StandaloneSearchBox.js
+++ client/ui/components/StandaloneSearchBox.js
@@ -1,82 +1,98 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const { compose, withProps, lifecycle } = require("recompose");
-const {
-  withScriptjs,
-} = require("react-google-maps");
-const { StandaloneSearchBox } = require("react-google-maps/lib/components/places/StandaloneSearchBox");
+import { get } from 'lodash';
+import { compose, withProps, lifecycle } from 'recompose';
+import { withScriptjs } from 'react-google-maps';
+import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox';
+import {buildBoundsRange, buildMarkerObj} from "../../../imports/helpers/DataHelpers";
 
 const PlacesWithStandaloneSearchBox = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDB1VkVvNXQUiKRzjVJoWfsyrusO5pkAWE&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${Meteor.settings.public.googleKeys.googleMapsApiKey}&v=3.exp&libraries=geometry,drawing,places`,
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
     }),
+    withScriptjs,
     lifecycle({
         componentWillMount() {
-            const refs = {}
+            const refs = {};
+            const { lat, lng } = this.props.coords || (this.props.place && this.props.place.coords) || { lat: 41.9, lng: -87.624 };
+            const boundPoints = buildBoundsRange({ lat, lng }, window.google.maps.LatLng, 3.5);
 
             this.setState({
                 places: [],
+                bounds: new window.google.maps.LatLngBounds(boundPoints.sw, boundPoints.ne),
+                center: {
+                    lat: lat || 41.9,
+                    lng: lng || -87.624,
+                },
                 onSearchBoxMounted: ref => {
                     refs.searchBox = ref;
                 },
                 onPlacesChanged: () => {
                     const places = refs.searchBox.getPlaces();
+                    if (this.props.onSearchComplete) this.props.onSearchComplete(places);
+
+                    this.state.setNewCenter(places);
+                },
+                setNewCenter: (places) => {
+                    const nextMarkers = places.map(place => ({
+                        position: place.geometry.location,
+                    }));
+                    const nextCenter = get(nextMarkers, '0.position', this.state.center);
+                    const boundsCenter = {
+                        lat: typeof nextCenter.lat === 'function' ? nextCenter.lat() : nextCenter.lat,
+                        lng: typeof nextCenter.lng === 'function' ? nextCenter.lng() : nextCenter.lng,
+                    };
+                    const { sw, ne } = buildBoundsRange(boundsCenter, window.google.maps.LatLng, 3.5);
 
                     this.setState({
-                        places,
+                        bounds: new window.google.maps.LatLngBounds(sw, ne),
+                        center: nextCenter,
+                        markers: nextMarkers,
                     });
                 },
-            })
+            });
         },
-    }),
-    withScriptjs
-)(props =>
-    <div data-standalone-searchbox="">
-        <StandaloneSearchBox
-            ref={props.onSearchBoxMounted}
-            bounds={props.bounds}
-            onPlacesChanged={props.onPlacesChanged}
-        >
-            {/* <div className="col s6 m3 input-field inline">
-                <input type="text" className="" id="search-box" style={{ textOverflow: 'ellipses' }} />
-                <label htmlFor="range-cap"><i className="fas fa-map-marker-alt" aria-hidden="true" />Search Listings</label>
-            </div> */}
-            <input
-                type="text"
-                placeholder="Search Listings"
-                style={{
-                    boxSizing: `border-box`,
-                    border: `1px solid transparent`,
-                    width: `240px`,
-                    height: `32px`,
-                    padding: `0 12px`,
-                    borderRadius: `3px`,
-                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                    fontSize: `14px`,
-                    outline: `none`,
-                    textOverflow: `ellipses`,
-                }}
-            />
-        </StandaloneSearchBox>
-        <ol>
-            {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
-                <li key={place_id}>
-                    {formatted_address}
-                    {" at "}
-                    ({location.lat()}, {location.lng()})
-        </li>
-            )}
-        </ol>
-    </div>
-    );
-
-<PlacesWithStandaloneSearchBox />
+    })
+)(props => {
+    const { bounds } = props;
+    return (
+        <div data-standalone-searchbox="" className={props.className}>
+            <StandaloneSearchBox
+                ref={props.onSearchBoxMounted}
+                bounds={bounds}
+                onPlacesChanged={props.onPlacesChanged}
+            >
+                {/* <div className="col s6 m3 input-field inline">
+                    <input type="text" className="" id="search-box" style={{ textOverflow: 'ellipses' }} />
+                    <label htmlFor="range-cap"><i className="fas fa-map-marker-alt" aria-hidden="true" />Search Listings</label>
+                </div> */}
+                <input
+                    type="text"
+                    placeholder="Search Listings"
+                    style={{
+                        boxSizing: `border-box`,
+                        border: `1px solid transparent`,
+                        width: `240px`,
+                        height: `32px`,
+                        padding: `0 12px`,
+                        borderRadius: `3px`,
+                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                        fontSize: `14px`,
+                        outline: `none`,
+                        textOverflow: `ellipses`,
+                    }}
+                />
+            </StandaloneSearchBox>
+        </div>);
+});
 
 PlacesWithStandaloneSearchBox.propTypes = {
     latitude: PropTypes.string,
     longitude: PropTypes.string,
+    onSearchComplete: PropTypes.func,
+    className: PropTypes.string,
 };
 
 export default PlacesWithStandaloneSearchBox;

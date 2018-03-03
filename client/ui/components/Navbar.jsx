48542eee8df7704e9ed4cf8dde3e06ca15088b90
takoda117 <takoda117@gmail.com>
2018-03-03 15:24:13 -0500

    Cleaned up navbar active styling and moved signupmodal content into its own component.

--- client/ui/components/Navbar.jsx
+++ client/ui/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link, withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import FontIcon from 'material-ui/FontIcon';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Login from '../pages/Login';
 import { defaultImageUrls } from '../../../imports/lib/Constants';
+import ModalActions from '../actions/ModalActions';
 
 const ACTIVE = { borderBottom: 'rgb(0, 188, 212) solid 5px' };
 
@@ -46,8 +49,19 @@ class Navbar extends React.Component {
         this.props.router.push('/');
     }
 
+    ifCurrentPathReturnStyle(pathName, router) {
+        if (router.getCurrentLocation().pathname.indexOf(pathName) !== -1) return ACTIVE;
+        return {};
+    }
+
+    transitionIfLoggedIn(path) {
+        const goToPathFunc = () => this.props.router.push(path);
+        if (this.props.user.userId) return goToPathFunc();
+        return this.props.modalActions
+    }
+
     render() {
-        const { className } = this.props;
+        const { className, router } = this.props;
         const invisToggle = this.isInvisRoute(this.props);
         return (
             <div className={`navbar-fixed ${className} ${invisToggle ? 'invis-toggle-true ' : ' '}${this.state.isTop ? 'invisible' : 'visible'}`} >
@@ -57,11 +71,11 @@ class Navbar extends React.Component {
                         <a href="#" data-activates="mobile-demo" className="button-collapse"><FontIcon className="material-icons">menu</FontIcon></a>
                         <Login className="nav-login border-nav" />
                         <ul className="right hide-on-med-and-down">
-                            <li style={{ color: 'black' }} className="border-nav"><Link to="/home" activeStyle={ACTIVE}>How It Works</Link></li>
-                            <li style={{ color: 'black' }} className="border-nav"><Link to="/faq" activeStyle={ACTIVE}>FAQ</Link></li>
-                            <li style={{ color: 'black' }} className="border-nav"><Link to="/planner" activeStyle={ACTIVE}>Planner</Link></li>
-                            <li style={{ color: 'black' }} className="border-nav explore-container"><span className="explore"><Link to="/explore" activeStyle={ACTIVE}>Explore</Link></span></li>
-                            <li style={{ color: 'black' }} className="border-nav"><Link to="/profile" activeStyle={ACTIVE}>Profile</Link></li>
+                            <li className="border-nav"><Link to="/home" activeStyle={ACTIVE}>How It Works</Link></li>
+                            <li className="border-nav"><Link to="/faq" activeStyle={ACTIVE}>FAQ</Link></li>
+                            <li className="border-nav"><Link style={this.ifCurrentPathReturnStyle('/planner', router)} to="" onClick={() => this.transitionIfLoggedIn('/planner')}>Planner</Link></li>
+                            <li className="border-nav explore-container" ><div style={this.ifCurrentPathReturnStyle('/explore', router)} ><span className="explore"><Link to="/explore" >Explore</Link></span></div></li>
+                            <li className="border-nav"><Link to="/profile" activeStyle={ACTIVE}>Profile</Link></li>
                         </ul>
                         <ul className="side-nav" id="mobile-demo">
                             <li><Link to="/home" activeStyle={ACTIVE}>How It Works</Link></li>
@@ -82,6 +96,8 @@ Navbar.propTypes = {
     router: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
     className: PropTypes.string,
+    user: PropTypes.object.isRequired,
+    modalActions: PropTypes.object.isRequired,
     invisToggle: PropTypes.bool,
 };
 
@@ -89,5 +105,17 @@ Navbar.defaultProps = {
     className: '',
     invisToggle: false,
 };
+function mapStateToProps(state) {
+    const { user } = state;
+    return {
+        user,
+    };
+}
 
-export default withRouter(Navbar);
+function mapDispatchToProps(dispatch) {
+    return {
+        modalActions: bindActionCreators(ModalActions, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navbar));

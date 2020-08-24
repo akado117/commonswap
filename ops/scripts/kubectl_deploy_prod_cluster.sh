452abd2963829e6692de4871f5aa12bb2a115fe1
akado117 <akaidan@mtu.edu>
2020-08-24 13:43:41 -0400

    Updated deployment scripts to support staging/prod env Updated secret names to support multi server

--- ops/scripts/kubectl_deploy_prod_cluster.sh
+++ ops/scripts/kubectl_deploy_prod_cluster.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-sed -i'.old' -e "s/%PROJECT_ID%/$GOOGLE_CLOUD_PROJECT/g" "$UROOT_PATH/ops/kubectl/meteor_deployment.yaml"
 
-kubectl create -f $UROOT_PATH/ops/kubectl/meteor_deployment.yaml
-kubectl create -f $UROOT_PATH/ops/kubectl/meteor_service.yaml
+DP_ENV=${1:-staging}
+
+# variable replacement within deployment yaml
+sed -e "s/%PROJECT_ID%/$GOOGLE_CLOUD_PROJECT/g" \
+-e "s/%ENV%/$DP_ENV/g" \
+"$UROOT_PATH/ops/kubectl/meteor_deployment.yaml" > "$UROOT_PATH/ops/kubectl/meteor_deployment_$DP_ENV.yaml"
+
+# variable replacement within service yaml
+sed -e "s/%ENV%/$DP_ENV/g" \
+"$UROOT_PATH/ops/kubectl/meteor_service.yaml" > "$UROOT_PATH/ops/kubectl/meteor_service_$DP_ENV.yaml"
+
+kubectl create -f "$UROOT_PATH/ops/kubectl/meteor_deployment_$DP_ENV.yaml"
+kubectl create -f "$UROOT_PATH/ops/kubectl/meteor_service_$DP_ENV.yaml"
--- ops/scripts/kubectl_set_secrets.sh
+++ ops/scripts/kubectl_set_secrets.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-MONGO_URL=$(gcloud secrets versions access latest --secret=cs-prod-mongo-url --format='get(payload.data)' | tr '_-' '/+' | base64 -d)
-ROOT_URL="https://www.commonswap.com"
+# staging/prod
+DP_ENV=${1:-staging}
+# staging/www
+SUBDOMAIN=${2:-staging}
 
+function get_from_gcp_secrets() {
+  SEC_NAME=$1
+  # Controlled in gcp secret  store
+  SEC_KEY="cs-$DP_ENV-$SEC_NAME"
+  echo $(gcloud secrets versions access latest --secret=$SEC_KEY --format='get(payload.data)' | tr '_-' '/+' | base64 -d)
+}
 
-kubectl create secret generic cs-prod-sec --from-literal=root_url="$ROOT_URL" --from-literal=db_url="$MONGO_URL"
+MONGO_URL=$(get_from_gcp_secrets mongo-url)
+METEOR_SETTINGS=$(get_from_gcp_secrets settings)
+ROOT_URL="https://$SUBDOMAIN.commonswap.com"
+
+# Create kube secret on cluste
+kubectl create secret generic cs-$DP_ENV-sec \
+--from-literal=root_url="$ROOT_URL" \
+--from-literal=db_url="$MONGO_URL" \
+--from-literal=meteor_settings="$METEOR_SETTINGS"
